4336da2532d07a9289908d194064c21b932a39e0
samkodes <uhwuhna@gmail.com>
2017-02-12 00:00:36 -0500

    quality and convenience fixes
    Fixed crashes; added re-ping on empty data; added poll_id and
    request_time to db; added poll table to db with start and end time of
    polls; more detailed file and console logging.

--- create_tables.sql
+++ create_tables.sql
@@ -2,13 +2,12 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.5
--- Dumped by pg_dump version 9.5.5
-
--- Started on 2017-02-05 23:10:21 EST
+-- Dumped from database version 9.6.1
+-- Dumped by pg_dump version 9.6.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -16,7 +15,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 1 (class 3079 OID 12990)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -24,8 +22,6 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2742 (class 0 OID 0)
--- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
 --
 
@@ -39,7 +35,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 183 (class 1259 OID 22878)
 -- Name: ntas_data; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -57,7 +52,36 @@ CREATE TABLE ntas_data (
 
 
 --
--- TOC entry 182 (class 1259 OID 22871)
+-- Name: polls; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE polls (
+    pollid integer NOT NULL,
+    poll_start timestamp without time zone,
+    poll_end timestamp without time zone
+);
+
+
+--
+-- Name: polls_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE polls_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: polls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE polls_id_seq OWNED BY polls.pollid;
+
+
+--
 -- Name: requests; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -67,12 +91,13 @@ CREATE TABLE requests (
     stationid integer NOT NULL,
     lineid integer NOT NULL,
     all_stations text,
-    create_date timestamp without time zone
+    create_date timestamp without time zone,
+    pollid integer,
+    request_date timestamp without time zone
 );
 
 
 --
--- TOC entry 181 (class 1259 OID 22869)
 -- Name: requests_requestid_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -85,8 +110,6 @@ CREATE SEQUENCE requests_requestid_seq
 
 
 --
--- TOC entry 2743 (class 0 OID 0)
--- Dependencies: 181
 -- Name: requests_requestid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -94,26 +117,26 @@ ALTER SEQUENCE requests_requestid_seq OWNED BY requests.requestid;
 
 
 --
--- TOC entry 2620 (class 2604 OID 22874)
--- Name: requestid; Type: DEFAULT; Schema: public; Owner: -
+-- Name: polls pollid; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY polls ALTER COLUMN pollid SET DEFAULT nextval('polls_id_seq'::regclass);
+
+
+--
+-- Name: requests requestid; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY requests ALTER COLUMN requestid SET DEFAULT nextval('requests_requestid_seq'::regclass);
 
 
 --
--- TOC entry 2741 (class 0 OID 0)
--- Dependencies: 6
--- Name: public; Type: ACL; Schema: -; Owner: -
+-- Name: polls polls_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
+ALTER TABLE ONLY polls
+    ADD CONSTRAINT polls_pkey PRIMARY KEY (pollid);
 
-
--- Completed on 2017-02-05 23:10:21 EST
 
 --
 -- PostgreSQL database dump complete
--- ttc_api_scraper.py
+++ ttc_api_scraper.py
@@ -2,6 +2,9 @@ import logging
 import sys
 import requests #to handle http requests to the API 
 from psycopg2 import connect #Connect to local PostgreSQL db
+from datetime import datetime
+from time import sleep
+#import socket
 
 #Not used anymore
 class MissingDataException( TypeError):
@@ -26,28 +29,41 @@ class TTCSubwayScraper( object ):
         payload = {"subwayLine":line_id,
                    "stationId":station_id,
                    "searchCriteria":''}
-        r = requests.get(self.BASE_URL, params = payload)
+        
+        # with timeout set to 10, need to use a try block here to catch timeout errors
+        try:
+            r = requests.get(self.BASE_URL, params = payload, timeout = 10)
+        except requests.exceptions.RequestException as err:
+            self.logger.critical(err)
+            return None
+        
+        # another try block will check for http error codes
         try:
             r.raise_for_status()
-        except request.exceptions.HTTPError as err:
+        except requests.exceptions.RequestException as err:
             self.logger.critical(err)
-            sys.exit(0)
+            return None
+
         return r.json()
 
-    def insert_request_info(self, data, line_id, station_id):
+    def insert_request_info(self, poll_id, data, line_id, station_id, request_date):
         request_row = {}
+        request_row['pollid'] = poll_id
+        request_row['request_date'] = request_date
         request_row['data_'] = data['data']
         request_row['stationid'] = station_id
         request_row['lineid'] = line_id
         request_row['all_stations'] = data['allStations']
         request_row['create_date'] = data['ntasData'][0]['createDate'].replace('T', ' ')
         cursor = self.con.cursor()
-        cursor.execute("INSERT INTO public.requests(data_, stationid, lineid, all_stations, create_date)"
-                       "VALUES(%(data_)s, %(stationid)s, %(lineid)s, %(all_stations)s, %(create_date)s)"
+        cursor.execute("INSERT INTO public.requests(data_, stationid, lineid, all_stations, create_date, pollid, request_date)"
+                       "VALUES(%(data_)s, %(stationid)s, %(lineid)s, %(all_stations)s, %(create_date)s, %(pollid)s, %(request_date)s)"
                        "RETURNING requestid", request_row)
         request_id = cursor.fetchone()[0] 
         self.con.commit()
         cursor.close()
+        self.logger.debug("Request " + str(request_id) + ": " + str(request_row) )
+
         return request_id
 
     def insert_ntas_data(self, ntas_data, request_id):
@@ -68,24 +84,68 @@ class TTCSubwayScraper( object ):
         self.con.commit()
         cursor.close()
 
+    def insert_poll_start(self, time):
+        cursor = self.con.cursor()
+        cursor.execute("INSERT INTO public.polls(poll_start)"
+                        "VALUES(%s)"
+                        "RETURNING pollid", (str(time),))
+        poll_id = cursor.fetchone()[0]
+        self.con.commit()
+        cursor.close()
+        self.logger.debug("Poll " + str(poll_id) + " started at " + str(time) )
+        return poll_id
+
+    def update_poll_end(self, poll_id, time):
+        cursor = self.con.cursor()
+        cursor.execute("UPDATE public.polls set poll_end = %s"
+                        "WHERE pollid = %s", (str(time), str(poll_id)) )
+        self.con.commit()
+        cursor.close()
+        self.logger.debug("Poll " + str(poll_id) + " ended at " + str(time) )
+
+
     def query_all_stations(self):
-        
+        poll_id = self.insert_poll_start( datetime.now() )
+        retries = 3
         for line_id, stations in self.LINES.items():
             for station_id in stations:
-                data = self.get_API_response(line_id, station_id)
-                if data.get('ntasData', None) is None or data.get('ntasData', None) == []:
+                for attempt in range(retries):
+                    data = self.get_API_response(line_id, station_id)
+                    if not ( data is None or data.get('ntasData', None) is None or data.get('ntasData', None) == []):
+                        break
+                    else:
+                        if data is None:
+                            # for http and timeout errors, sleep 2s before retrying
+                            self.logger.debug("Sleeping 2s for request error ...")
+                            sleep(2)
+
+                        self.logger.debug("Try " + str(attempt+1) + " for station " + str(station_id) + " failed.")
+                            
+
+                if data is None or data.get('ntasData', None) is None or data.get('ntasData', None) == []:
                     errmsg = 'No data for line {line}, station {station}'
                     self.logger.error(errmsg.format(line=line_id, station=station_id))
-                    continue
-                request_id = self.insert_request_info(data, line_id, station_id)
+                    self.logger.debug( errmsg.format(line=line_id, station=station_id) )
+                    continue    
+                request_id = self.insert_request_info(poll_id, data, line_id, station_id, datetime.now() )
                 self.insert_ntas_data(data['ntasData'], request_id)
+
+        self.update_poll_end( poll_id, datetime.now() )
 
 if __name__ == '__main__':
     FORMAT = '%(asctime)-15s %(message)s'
-    logging.basicConfig(level=logging.ERROR, format=FORMAT, filename='scraper.log')
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT, filename='scraper.log')
     LOGGER = logging.getLogger(__name__)
+
+    # add console output for debugging
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    LOGGER.addHandler(ch)
+
     con = connect(database='ttc',
-                  user='rad')
+                  user='postgres')  # adjust for local setup
     scraper = TTCSubwayScraper(LOGGER, con)
     scraper.query_all_stations()
     con.close()
